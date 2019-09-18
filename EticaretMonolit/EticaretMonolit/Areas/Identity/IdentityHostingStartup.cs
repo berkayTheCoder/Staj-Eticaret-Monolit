a7d10a481a3873c85f4f08c43eb8464f8504c3ab
şükrü berkay <berkay_1071@hotmail.com>
2019-09-18 12:54:14 +0300

    1-2-3-4 günler

---
+++ EticaretMonolit/EticaretMonolit/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EticaretMonolit.Areas.Identity.Data;
+using EticaretMonolit.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EticaretMonolit.Areas.Identity.IdentityHostingStartup))]
+namespace EticaretMonolit.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EticaretMonolitContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("EticaretMonolitContextConnection")));
+
+                services.AddDefaultIdentity<EticaretMonolitUser>()
+                    .AddEntityFrameworkStores<EticaretMonolitContext>();
+            });
+        }
+    }
+}
